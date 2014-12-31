7a33063ea226547afd467d375131fe0ced209b2d
hudson <hudson@192.168.0.1>
2014-12-31 12:42:55 -0200

    #Set Server manaully

--- src/br/com/schumaker/gfx/FrMain.java
+++ src/br/com/schumaker/gfx/FrMain.java
@@ -10,25 +10,25 @@ import javax.swing.DefaultListModel;
  * @author hudson schumaker
  */
 public class FrMain extends javax.swing.JFrame {
-
+    
     private DefaultListModel lista = new DefaultListModel();
-
+    
     public FrMain() {
         initComponents();
         jList.setModel(lista);
         setLocationRelativeTo(null);
     }
-
+    
     public void setMessage(String sms) {
         jTextArea.append(sms);
         jTextArea.append("\n");
         jTextArea.setCaretPosition(jTextArea.getDocument().getLength());
     }
-
+    
     public void addToList(String s) {
         lista.addElement(s);
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -45,6 +45,7 @@ public class FrMain extends javax.swing.JFrame {
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -109,6 +110,14 @@ public class FrMain extends javax.swing.JFrame {
 
         jMenu2.setText("Edit");
 
+        jMenuItem3.setText("Server");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem3);
+
         jMenuItem2.setText("Scan Server");
         jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -136,7 +145,7 @@ public class FrMain extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldActionPerformed
-
+        
         Engine.getInstance().sendMessage(Encryption.encrypt(WelcomeSmsCreator.me() + ": " + jTextField.getText()));
         jTextField.setText("");
     }//GEN-LAST:event_jTextFieldActionPerformed
@@ -149,6 +158,10 @@ public class FrMain extends javax.swing.JFrame {
         Engine.getInstance().scanServer();
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        new FrServer().setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JList jList;
     private javax.swing.JMenu jMenu1;
@@ -156,6 +169,7 @@ public class FrMain extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
---
+++ src/br/com/schumaker/gfx/FrServer.java
@@ -0,0 +1,75 @@
+package br.com.schumaker.gfx;
+
+import br.com.schumaker.core.Engine;
+
+/**
+ *
+ * @author hudson schumaker
+ */
+public class FrServer extends javax.swing.JFrame {
+    
+    public FrServer() {
+        initComponents();
+        jTextFieldIpServer.setText(Engine.getInstance().getServer());
+        setLocationRelativeTo(null);
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jTextFieldIpServer = new javax.swing.JTextField();
+        jButtonSave = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Set a server");
+        setResizable(false);
+
+        jLabel1.setText("Type a server address:");
+
+        jButtonSave.setText("Save");
+        jButtonSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSaveActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jTextFieldIpServer, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonSave))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextFieldIpServer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButtonSave))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSaveActionPerformed
+        Engine.getInstance().setServer(jTextFieldIpServer.getText());
+    }//GEN-LAST:event_jButtonSaveActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonSave;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField jTextFieldIpServer;
+    // End of variables declaration//GEN-END:variables
+}
